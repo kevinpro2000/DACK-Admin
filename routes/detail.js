c2cc7d35896eb44dfe465ac8e6d0821c9559e110
kevinpro2000 <kevinpro2000@gmail.com>
2020-12-20 17:45:26 +0700

    lol

--- routes/detail.js
+++ routes/detail.js
@@ -2,11 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 const detailController = require('../controller/detailController');
+const productController = require('../controller/productController');
 
 router.get('/', detailController.index);
 
 router.get('/turn-back', (req, res) => {
     res.redirect('/');
 });
+
+router.get('/delete', productController.delete);
 
 module.exports = router;
